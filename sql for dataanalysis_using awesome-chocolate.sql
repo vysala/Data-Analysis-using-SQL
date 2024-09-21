494ac2041e84df5b12b2dc84670d711fb2df9a42
vysala <42871943+vysala@users.noreply.github.com>
2024-09-21 11:08:36 -0700

    Add files via upload

---
+++ sql for dataanalysis_using awesome-chocolate.sql
@@ -0,0 +1,101 @@
+Use `awesome chocolates`;
+
+-- Select all data from sales table
+
+select * from sales;
+
+-- Show only the selected columns from sales table
+
+select SaleDate, Amount, Customers from sales;
+
+select Amount, Customers, GeoID from sales;
+
+-- Add a calculated column with SQL, Name the new field as 'Total per box' 
+
+Select SaleDate, Amount, Boxes, Amount / boxes as 'Total per box'  from sales;
+
+-- Using WHERE Clause in SQL
+
+select * from sales
+where amount > 10000;
+
+-- Showing sales data where amount is greater than 10,000 by descending order
+
+select * from sales
+where amount > 10000
+order by amount desc;
+
+-- Showing sales data where GEOID is g1 and order by product ID &  order of amounts desc
+select * from sales
+where geoid='g1'
+order by PID, Amount desc;
+
+-- Working with dates in SQL
+
+Select * from sales
+where amount > 10000 and SaleDate >= '2022-01-01';
+
+-- Using year() function to select all data in a specific year
+
+select SaleDate, Amount from sales
+where amount > 10000 and year(SaleDate) = 2022
+order by amount desc;
+
+-- BETWEEN condition in SQL with < & > operators
+
+select * from sales
+where boxes >0 and boxes <=50;
+
+-- Using the between operator in SQL
+
+
+select * from sales
+where boxes between 0 and 50;
+
+-- Using weekday() function in SQL. 
+-- The WEEKDAY() function returns the weekday number for a given date.
+-- Note: 0 = Monday, 1 = Tuesday, 2 = Wednesday, 3 = Thursday, 4 = Friday, 5 = Saturday, 6 = Sunday.
+
+select SaleDate, Amount, Boxes, weekday(SaleDate) as 'Day of week'
+from sales ;
+
+select SaleDate, Amount, Boxes, weekday(SaleDate) as 'Day of week'
+from sales
+where weekday(SaleDate) = 4;
+
+-- Let's Work with People table now
+
+select * from people;
+
+-- OR operator in SQL
+
+select * from people
+where team = 'Delish' or team = 'Jucies';
+
+-- IN operator in SQL
+
+select * from people
+where team in ('Delish','Jucies');
+
+-- LIKE operator in SQL
+
+select * from people
+where salesperson like 'B%';
+
+select * from people
+where salesperson like '%B%';
+
+select * from sales;
+
+-- Using CASE to create branching logic in SQL
+
+select 	SaleDate, Amount,
+		case 	when amount < 1000 then 'Under 1k'
+				when amount < 5000 then 'Under 5k'
+                when amount < 10000 then 'Under 10k'
+			else '10k or more'
+		end as 'Amount category'
+from sales;
+
+select team, count(*) from people
+group by team;
