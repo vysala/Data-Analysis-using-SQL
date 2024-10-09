12f3c0a0766b14200e3ce3d095b4a8969f0bd4c0
APV <42871943+vysala@users.noreply.github.com>
2024-10-09 09:16:36 -0700

    Update sql for dataanalysis_using awesome-chocolate.sql

--- sql for dataanalysis_using awesome-chocolate.sql
+++ sql for dataanalysis_using awesome-chocolate.sql
@@ -1,56 +1,52 @@
 Use `awesome chocolates`;
 
--- Select all data from sales table
+-- Retrieve all records from sales table
 
 select * from sales;
 
--- Show only the selected columns from sales table
+-- Retrieve selected columns from sales table
 
 select SaleDate, Amount, Customers from sales;
 
 select Amount, Customers, GeoID from sales;
 
--- Add a calculated column with SQL, Name the new field as 'Total per box' 
+-- Add a calculated column with SQL and name the new column as 'Total per box' 
 
 Select SaleDate, Amount, Boxes, Amount / boxes as 'Total per box'  from sales;
 
--- Using WHERE Clause in SQL
+-- Using WHERE Clause in SQL to restrict the rows in the result
 
 select * from sales
 where amount > 10000;
 
--- Showing sales data where amount is greater than 10,000 by descending order
+-- Retrieve only those sales data where amount is greater than 10,000 and order them in descending order
 
 select * from sales
 where amount > 10000
 order by amount desc;
 
--- Showing sales data where GEOID is g1 and order by product ID &  order of amounts desc
+-- Retrieve sales data where GEOID is g1 and order by product ID &  order of amounts desc
+
 select * from sales
 where geoid='g1'
 order by PID, Amount desc;
 
--- Working with dates in SQL
+-- Retrieve records from sales table where sales date is after '2022-01-01' and amount >10000 
 
 Select * from sales
 where amount > 10000 and SaleDate >= '2022-01-01';
 
--- Using year() function to select all data in a specific year
+-- Find out the sales data for the year 2022 if the sales amount is greater than 10000. Use year() function to retrieve data in a specific year
 
 select SaleDate, Amount from sales
 where amount > 10000 and year(SaleDate) = 2022
 order by amount desc;
 
--- BETWEEN condition in SQL with < & > operators
+-- BETWEEN operator in SQL with < & > operators
+--Retrieve sales data for boxes between 1 and 50.  
 
 select * from sales
 where boxes >0 and boxes <=50;
-
--- Using the between operator in SQL
-
-
-select * from sales
-where boxes between 0 and 50;
 
 -- Using weekday() function in SQL. 
 -- The WEEKDAY() function returns the weekday number for a given date.
@@ -59,11 +55,12 @@ where boxes between 0 and 50;
 select SaleDate, Amount, Boxes, weekday(SaleDate) as 'Day of week'
 from sales ;
 
+-- Using weekday() function in SQL with where clause . 
 select SaleDate, Amount, Boxes, weekday(SaleDate) as 'Day of week'
 from sales
 where weekday(SaleDate) = 4;
 
--- Let's Work with People table now
+--Queries to work with People table 
 
 select * from people;
 
