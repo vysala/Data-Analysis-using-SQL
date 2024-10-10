d251fce908969b1ed83224f7f8bf33a86b4f333c
APV <42871943+vysala@users.noreply.github.com>
2024-10-10 12:16:34 -0700

    Update sql for dataanalysis_using awesome-chocolate.sql

--- sql for dataanalysis_using awesome-chocolate.sql
+++ sql for dataanalysis_using awesome-chocolate.sql
@@ -101,3 +101,99 @@ from sales;
 -- Find the number of people in each team 
 select team, count(*) from people
 group by team;
+
+-- To see sales data with salesperson's name, we have to JOIN sales and People's table.
+-- The common column between both tables is the salesperson ID (SPID).
+SELECT s.SaleDate, s.Amount, p.Salesperson, s.SPID
+	FROM Sales s
+	JOIN People p ON p.SPID = s.SPID;
+    
+    -- Analyzing products sale by joining Sales and Products tables. I have performed a left join here as I only need to analyze the products that have been sold.
+SELECT s.SaleDate, s.Amount, s.PID, p.Product
+	FROM Sales s
+	LEFT JOIN Products p ON p.PID = s.PID;
+    
+    
+--  multiple table Joining - sales, people and products
+SELECT s.SaleDate, s.Amount, p.Product, e.Salesperson, e.Team
+	FROM Sales s
+	JOIN People e ON s.SPID = s.SPID
+	JOIN Products p ON p.PID = s.PID;    
+    
+-- Filtering multiple jointed table for retrieving Delish team's sales where amount is less than 1000
+SELECT Sales.SaleDate, Sales.Amount, Products.Product, People.Salesperson, People.Team
+	FROM Sales
+	JOIN People ON People.SPID = Sales.SPID
+	JOIN Products ON Products.PID = Sales.PID
+	WHERE People.Team = 'Delish' AND Sales.Amount < 1000;
+
+
+
+-- Filtering multiple joined table for salespersons without teams, sales less than 1000
+SELECT Sales.SaleDate, Sales.Amount, Products.Product, People.Salesperson, People.Team
+	FROM Sales
+	JOIN People ON People.SPID = Sales.SPID
+	JOIN Products ON Products.PID = Sales.PID
+	WHERE People.Team = '' AND Sales.Amount < 1000;
+/*
+We can use WHERE People.Team IS NULL, but in this awesome chocolates database the Team values which
+are empty are not marked as NULL, which in SQL will appear as a small gray box within
+the field. In this database, they are left blank, and not NULL. So we just use quote marks
+'' with no characters between them, to filter for blank fields in Team column.
+*/
+
+-- Filtering above query for teamless sales, less than 1000, shipped to NZ or India
+SELECT Sales.SaleDate, Sales.Amount, Products.Product, People.Salesperson, People.Team
+	FROM Sales
+	JOIN People ON People.SPID = Sales.SPID
+	JOIN Products ON Products.PID = Sales.PID
+	JOIN Geo ON Geo.GeoID = Sales.GeoID
+	WHERE People.Team = '' AND Sales.Amount < 1000
+	AND Geo.Geo IN ('New Zealand','India')
+	ORDER BY SaleDate;
+/*
+Note that there is no 'Geo' column in the results. This is because, though we have
+joined the Geo table and included a filter for the Geo.Geo column, we have not
+SELECTed the Geo.Geo column to be displayed in the final results. So SQL will apply
+the filter, but not display the Geo.Geo column in the final results.
+*/
+
+-- GROUP BY GeoID to aggregate Sales Amounts by geographic region
+Select count(GeoID) from geo;   -- To check how many geo id's are there. 
+
+SELECT GeoID, sum(Amount), avg(Amount), sum(Boxes)
+	FROM Sales
+	GROUP BY GeoID;
+    
+-- Using group by on geo column to get the geographic name rather than the geoid. Joining sales table and Geo table as we need the geographic names. 
+SELECT Geo.Geo, sum(Amount), avg(Amount), sum(Boxes)
+	FROM Sales
+	JOIN Geo ON Geo.GeoID = Sales.GeoID
+	GROUP BY Geo.Geo;
+    
+    
+    -- Grouping and sorting multiple joined tables for sales by team per product category . Ignore the teams that are empty. 
+SELECT p.Category, e.Team, sum(Boxes), sum(Amount)
+	FROM Sales s
+	JOIN People e ON e.SPID = s.SPID
+	JOIN Products p ON p.PID = s.PID
+	GROUP BY p.Category, e.Team
+    having e.Team <> ''
+	ORDER BY p.Category, e.Team;
+
+
+-- Retrieve Sales Amounts by product, filtering only Top 10 products
+SELECT p.Product, sum(Amount) AS 'Total Sales Amount'
+	FROM Sales s
+	JOIN Products p ON p.PID = s.PID
+	GROUP BY p.Product
+	ORDER BY `Total Sales Amount` DESC
+	LIMIT 10;
+    
+
+    
+    
+    
+    
+    
+
